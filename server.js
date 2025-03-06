c2334d201b6c525f9d3b67fdce1d23a248bc150b
CAhylA1 <cahylalouidor@gmail.com>
2025-03-06 17:13:18 -0500

    users.js file

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const app = express();
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+
 //Database Configuration
 mongoose.connect(process.env.DATABASE_URL);
+
+//Middleware
+//Body parser middleware: give us access to req.body
+app.use(express.urlencoded({ extended: true}))
+
 
 // Database Connection Error / Success
 const db = mongoose.connection;
 db.on("error", (err) => console.log(err.message + " is mongod not running?"));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo disconnected"));
+
+// Routes / Controllers
+const userController = require("./controllers/users")
+app.use("/users", userController)
+
+
 
 // Listener
 const PORT = process.env.PORT;
